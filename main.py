bb0466b0505bebbd095e9ae84fd760318486721d
pranav <pranav101010@gmail.com>
2024-03-30 10:53:34 +0530

    changes updated

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-def to_upper(name):
-    return name.upper()
+def say_hello():
+    return "Hello, World!"
 
+print(say_hello())
 
-def say_hello(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hello, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    name = 'Hello from Pranav'
-    say_hello(name)
-    up = to_upper(name)
-    print(up)
